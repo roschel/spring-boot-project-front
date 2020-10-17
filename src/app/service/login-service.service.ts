b69ef055b8baf522d2f1c3655e7fb6e3cb59d191
roschel <joaomroschel@gmail.com>
2020-10-17 11:17:59 -0300

    validando login

---
+++ src/app/service/login-service.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { Injectable } from '@angular/core';
+import { AppConstants } from '../app-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario){
+    return this.http.post(AppConstants.baseLogin, JSON.stringify(usuario)).subscribe(data => {
+      //corpo do retorno http
+      var token = JSON.parse(JSON.stringify(data)).Authorization.split(" ")[1]
+
+      localStorage.setItem("token", token);
+
+
+    },
+    error => {
+      console.error("Erro ao fazer login ", error);
+    }
+    )
+  }
+}
